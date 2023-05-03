6b0255f4864ace1b96b78fb8b738256d3c4b46f8
ScyBry <99564441+ScyBry@users.noreply.github.com>
2023-05-03 15:29:03 +0300

    update

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Stack, Box } from '@mui/material';
+import { VideoCard, ChannelCard } from '../components';
 
 function Videos({ videos }) {
-  return <div></div>;
+  console.log(videos);
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item}></VideoCard>}
+          {item.id.channelId && <ChannelCard channelDetail={item}></ChannelCard>}
+        </Box>
+      ))}
+    </Stack>
+  );
 }
 
 export default Videos;
--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,7 @@ export { default as Feed } from './Feed.jsx';
 export { default as VideoDetail } from './VideoDetail.jsx';
 export { default as Sidebar } from './Sidebar.jsx';
 export { default as Videos } from './Videos.jsx';
+export { default as ChannelCard } from './ChannelCard.jsx';
+export { default as VideoCard } from './VideoCard.jsx';
 
 // export {default as SearchBar} from './SearchBar.jsx'
